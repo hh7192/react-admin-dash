d7983b931fec4e0eec65e63b1f4ec2341167f794
HAMIDREZA HASHEMI <HH7192@gmail.com>
2022-05-23 18:47:01 +0430

    build new pages.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import List from "./pages/list/List";
 import Login from "./pages/login/Login";
 import New from "./pages/new/New";
 import Single from "./pages/single/Single";
+import {userInputs} from "./formSource";
+import {productInputs} from "./formSource";
 
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
@@ -17,12 +19,18 @@ function App() {
             <Route path='users'>
               <Route index element={<List />} />
               <Route path=':userID' element={<Single />} />
-              <Route path='new' element={<New />} />
+              <Route
+                path='new'
+                element={<New inputs={userInputs} title='Add New User' />}
+              />
             </Route>
             <Route path='products'>
               <Route index element={<List />} />
               <Route path=':productID' element={<Single />} />
-              <Route path='new' element={<New />} />
+              <Route
+                path='new'
+                element={<New inputs={productInputs} title='Add New Product' />}
+              />
             </Route>
           </Route>
         </Routes>
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,8 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
 import "./new.scss";
+import Sidebar from "../../componants/sidebar/Sidebar";
+import Navbar from "../../componants/navbar/Navbar";
+import IMG from "../../images/no-image.png";
+import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
 
-const New = () => {
-  return <div>New</div>;
+const New = ({inputs, title}) => {
+  const [file, setFile] = useState("");
+
+  return (
+    <div className='new'>
+      <Sidebar />
+      <div className='new__container'>
+        <Navbar />
+        <div className='top'>
+          <h1 className='title'>{title}</h1>
+        </div>
+        <div className='bottom'>
+          <div className='left'>
+            <img
+              src={file ? URL.createObjectURL(file) : IMG}
+              alt='no-image'
+              className='newImg'
+            />
+          </div>
+
+          <div className='right'>
+            <form className='form'>
+              <div className='input__container'>
+                <label htmlFor='file'>
+                  Image: <DriveFolderUploadIcon className='icon' />
+                </label>
+                <input
+                  type='file'
+                  id='file'
+                  className='input__file'
+                  onChange={(e) => setFile(e.target.files[0])}
+                />
+              </div>
+
+              {inputs.map((input) => (
+                <div className='input__container' key={input.id}>
+                  <label>{input.label}</label>
+                  <input
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    required
+                    minlength='4'
+                    maxlength='50'
+                    className='input__info'
+                  />
+                </div>
+              ))}
+
+              <button type='submit' className='btn'>
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default New;
